353b127ab83661d17b3067f51ea82ffde77e65ca
nileshsharma198 <trivedinilesh198@gmail.com>
2025-10-15 02:00:35 +0530

    cahnges in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,9 @@ import Home from './components/Home/Home';
 import BrandCarousel from './components/Products/BrandCarousel';
 import Reviews from './components/Reviews/Reviews';
 import Contact from './components/Contact/Contact';
-<<<<<<< HEAD
-=======
 import ProductsSection from './components/Products/ProductSection';
 import Footer from './components/Common/Footer';
 import FloatingButton from './components/Common/FloatingButton';
->>>>>>> 7551ef0 (First Commit)
 
 export default function App() {
   return (
@@ -17,16 +14,13 @@ export default function App() {
       <Header />
       <Home/>
       <BrandCarousel/>
-<<<<<<< HEAD
       <Reviews/>
       <Contact/>
-=======
       <ProductsSection/>
       <Reviews/>
       <Contact/>
       <Footer/>
       <FloatingButton/>
->>>>>>> 7551ef0 (First Commit)
     </>
   );
 }
